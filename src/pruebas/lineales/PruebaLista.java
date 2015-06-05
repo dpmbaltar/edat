939e21016f9c031beb76faf64fd9676c23b999fa
Diego P. M. Baltar <dpmbaltar@gmail.com>
2015-06-05 19:38:00 -0300

    Agregado prueba de implementación de Lista (pruebas.lineales).

--- src/pruebas/lineales/PruebaLista.java
+++ src/pruebas/lineales/PruebaLista.java
@@ -1,103 +1,131 @@
 package pruebas.lineales;
 
-import lineales.dinamicas.ListaInt;
+import lineales.dinamicas.Lista;
 
 /**
- * Implementación de Lista dinámica.
+ * Prueba implementación de Lista dinámica.
  * 
  * @author Diego P. M. Baltar <www.dpmbaltar.com.ar>
  */
 public class PruebaLista {
 
 	public static void main(String[] args) {
-		pruebaListaInt();
-		//pruebaEliminarImpares();
-	}
-	
-	public static void pruebaListaInt() {
-		ListaInt l1 = new ListaInt();
-		// Insertar 1, 2, 3, 4
-		System.out.println("Crear lista vacía (l1):");
-		System.out.println(l1);
-		System.out.println("Insertar elem. 1@1: "+l1.insertar(1, 1));
-		System.out.println(l1);
-		System.out.println("Insertar elem. 2@2: "+l1.insertar(2, 2));
-		System.out.println(l1);
-		System.out.println("Insertar elem. 3@3: "+l1.insertar(3, 3));
-		System.out.println(l1);
-		System.out.println("Insertar elem. 4@4: "+l1.insertar(4, 4));
-		System.out.println(l1);
-		// Eliminar 1 y 3
-		System.out.println("Eliminar elem. @1: "+l1.eliminar(1));
-		System.out.println(l1);
-		System.out.println("Eliminar elem. @2: "+l1.eliminar(2));
-		System.out.println(l1);
-		// Eliminar elemento inexistente en la lista
-		System.out.println("Eliminar elem. @5: "+l1.eliminar(5));
-		System.out.println(l1);
-		// Obtener cantidad de elementos
-		System.out.println("Longitud: "+l1.longitud());
-		System.out.println(l1);
-		System.out.println("Insertar elem. 10@3: "+l1.insertar(10, 3));
-		System.out.println(l1);
-		System.out.println("Insertar elem. 8@3: "+l1.insertar(8, 3));
-		System.out.println(l1);
-		System.out.println("Insertar elem. 6@3: "+l1.insertar(6, 3));
-		System.out.println(l1);
-		// Crear copia
-		ListaInt l2 = l1.clonar();
-		System.out.println("Crear copia de l1 (l2):");
-		System.out.println(l2);
-		// Localizar elemento en l2
-		System.out.println("Localizar elem. 6:");
-		System.out.println(l2.localizar(6));
-		System.out.println("Localizar elem. 2:");
-		System.out.println(l2.localizar(2));
-		System.out.println("Localizar elem. 10:");
-		System.out.println(l2.localizar(10));
-		// Obtener elem. @2, @5, @7
-		System.out.println("Recuperar elem. @2:");
-		System.out.println(l2.recuperar(2));
-		System.out.println("Recuperar elem. @5:");
-		System.out.println(l2.recuperar(5));
-		System.out.println("Recuperar elem. @7 (no existe):");
-		System.out.println(l2.recuperar(7));
-		// Modificar l1
-		System.out.println("Eliminar elem. @2: "+l1.eliminar(2));
-		System.out.println(l1);
-		System.out.println("Eliminar elem. @3: "+l1.eliminar(3));
-		System.out.println(l1);
-		System.out.println("Eliminar elem. @2: "+l1.eliminar(2));
-		System.out.println(l1);
-		// Modificar l2
-		System.out.println("Eliminar elem. @1: "+l2.eliminar(1));
-		System.out.println(l2);
-		System.out.println("Eliminar elem. @4: "+l2.eliminar(4));
-		System.out.println(l2);
-		// Vaciar l1
-		System.out.println("Vaciar l1:");
-		l1.vaciar();
-		System.out.println(l1);
-		// Vaciar l1
-		System.out.println("Vaciar l2:");
-		l2.vaciar();
-		System.out.println(l2);
-		// Obtener cantidad de elementos de l1
-		System.out.println("Longitud de l1: "+l1.longitud());
-		System.out.println(l1);
-		// Obtener cantidad de elementos de l2
-		System.out.println("Longitud de l1: "+l2.longitud());
-		System.out.println(l2);
-	}
-	
-	public static void pruebaEliminarImpares() {
-		ListaInt l1 = new ListaInt();
-		int i;
-		for (i = 0; i < 20; i++) {
-			l1.insertar(i, l1.longitud() + 1);
+		try {
+			pruebaInsertar();
+			pruebaRecuperar();
+			pruebaEliminar();
+			pruebaLocalizar();
+			pruebaLongitud();
+			pruebaEsVacia();
+			pruebaVaciar();
+			pruebaClonar();
+			pruebaToString();
+			System.out.println("Prueba OK");
+		} catch (AssertionError e) {
+			System.out.println("Error de prueba: "+e.getMessage());
+			e.printStackTrace();
 		}
-		
-		l1.eliminarImpares();
-		System.out.println(l1);
+	}
+	
+	protected static void pruebaInsertar() {
+		Lista<Integer> lista = new Lista<Integer>();
+		assert lista.insertar(1, 1)
+			 : "Insertar 1 en posición 1";
+		assert lista.insertar(2, -1) == false
+			 : "Insertar 2 en posición inválida (< 1)";
+		assert lista.insertar(2, 3) == false
+			 : "Insertar 2 en posición inválida (> longitud)";
+	}
+	
+	protected static void pruebaRecuperar() {
+		Lista<Integer> lista = new Lista<Integer>();
+		lista.insertar(1, 1);
+		lista.insertar(2, 2);
+		lista.insertar(3, 3);
+		assert lista.recuperar(3) == 3
+			 : "Recuperar elemento de la posición 3";
+	}
+	
+	protected static void pruebaEliminar() {
+		Lista<Integer> lista = new Lista<Integer>();
+		lista.insertar(1, 1);
+		lista.insertar(2, 2);
+		lista.insertar(3, 3);
+		assert lista.eliminar(3)
+			 : "Eliminar elemento en la posición 3";
+		assert lista.recuperar(3) == null
+			 : "Recuperar elemento eliminado (3)";
+	}
+	
+	protected static void pruebaLocalizar() {
+		Lista<Integer> lista = new Lista<Integer>();
+		lista.insertar(1, 1);
+		lista.insertar(2, 2);
+		lista.insertar(2, 3);
+		assert lista.localizar(2) == 2
+			 : "Localizar 2";
+		assert lista.localizar(3) == -1
+			 : "Localizar elemento inexistente (3)";
+	}
+	
+	protected static void pruebaLongitud() {
+		Lista<Integer> lista = new Lista<Integer>();
+		lista.insertar(1, 1);
+		lista.insertar(2, 2);
+		lista.insertar(3, 3);
+		assert lista.longitud() == 3
+			 : "Longitud debe ser 3";
+		lista.eliminar(3);
+		assert lista.longitud() == 2
+			 : "Longitud debe ser 2";
+		lista.eliminar(2);
+		lista.eliminar(1);
+		assert lista.longitud() == 0
+			 : "Longitud debe ser 0";
+	}
+	
+	protected static void pruebaEsVacia() {
+		Lista<Integer> lista = new Lista<Integer>();
+		assert lista.esVacia()
+			 : "Lista vacía";
+		lista.insertar(1, 1);
+		assert lista.esVacia() == false
+			 : "Lista no vacía";
+	}
+	
+	protected static void pruebaVaciar() {
+		Lista<Integer> lista = new Lista<Integer>();
+		lista.insertar(1, 1);
+		lista.insertar(2, 2);
+		lista.insertar(3, 3);
+		lista.vaciar();
+		assert lista.esVacia()
+			 : "Lista vacía";
+		assert lista.longitud() == 0
+			 : "Longitud debe ser 0";
+	}
+	
+	protected static void pruebaClonar() {
+		Lista<Integer> lista = new Lista<Integer>();
+		lista.insertar(1, 1);
+		lista.insertar(2, 2);
+		lista.insertar(3, 3);
+		Lista<Integer> clon = lista.clonar();
+		assert lista.recuperar(1) == clon.recuperar(1)
+			 : "Lista debe ser igual a su clon";
+		assert lista.recuperar(2) == clon.recuperar(2)
+			 : "Lista debe ser igual a su clon";
+		assert lista.recuperar(3) == clon.recuperar(3)
+			 : "Lista debe ser igual a su clon";
+	}
+	
+	protected static void pruebaToString() {
+		Lista<Integer> lista = new Lista<Integer>();
+		lista.insertar(1, 1);
+		lista.insertar(2, 2);
+		lista.insertar(3, 3);
+		assert lista.toString() == "[1 2 3]"
+			 : "Lista con elementos 1, 2, 3 debe ser representada como [1 2 3] "
+			 + "en forma de cadena";
 	}
 }
