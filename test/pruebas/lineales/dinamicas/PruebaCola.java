258d0c95fa8c44634747337ad0e20f1a37caebcc
Diego P. M. Baltar <dpmbaltar@gmail.com>
2016-03-27 22:56:38 -0300

    Cambiar implementación de Cola de caracteres a cadenas.

--- test/pruebas/lineales/dinamicas/PruebaCola.java
+++ test/pruebas/lineales/dinamicas/PruebaCola.java
@@ -3,13 +3,13 @@ package pruebas.lineales.dinamicas;
 import lineales.dinamicas.Cola;
 
 /**
- * Prueba implementación de Cola dinámica (de caracteres).
+ * Prueba implementación de Cola dinámica (de cadenas).
  *
  * @author Diego P. M. Baltar <dpmbaltar@gmail.com>
  */
 public class PruebaCola {
 
-    protected Cola<Character> cola;
+    protected Cola<String> cola;
 
     public PruebaCola() {
         preparar();
@@ -29,70 +29,97 @@ public class PruebaCola {
     }
 
     protected void preparar() {
-        cola = new Cola<Character>();
+        cola = new Cola<String>();
     }
 
+    /**
+     * Prueba lineales.dinamicas.Cola.poner().
+     */
     protected void pruebaPoner() {
-        assert cola.poner('a') : "Debe poner 'a' en el frente";
+        assert cola.poner("Uno") : "Debe poner \"Uno\"";
     }
 
+    /**
+     * Prueba lineales.dinamicas.Cola.obtenerFrente().
+     */
     protected void pruebaObtenerFrente() {
-        cola.poner('a');
-        cola.poner('b');
-        cola.poner('c');
-        assert cola.obtenerFrente() == 'a' : "Frente de cola debe ser 'a'";
+        assert cola.obtenerFrente() == null
+             : "Frente de cola vacía debe ser nulo";
+        cola.poner("Uno");
+        cola.poner("Dos");
+        cola.poner("Tres");
+        assert cola.obtenerFrente().equals("Uno")
+             : "Frente de cola debe ser \"Uno\"";
     }
 
+    /**
+     * Prueba lineales.dinamicas.Cola.sacar().
+     */
     protected void pruebaSacar() {
-        cola.poner('a');
-        cola.poner('b');
-        cola.poner('c');
-        assert cola.sacar() : "Debe sacar 'a' de la cola";
-        assert cola.obtenerFrente() == 'b' : "Frente de cola debe ser 'b'";
+        assert !cola.sacar() : "No debe sacar de cola vacía";
+        cola.poner("Uno");
+        cola.poner("Dos");
+        cola.poner("Tres");
+        assert cola.sacar() : "Debe sacar \"Uno\"";
+        assert cola.obtenerFrente().equals("Dos")
+             : "Frente de cola debe ser \"Dos\"";
     }
 
+    /**
+     * Prueba lineales.dinamicas.Cola.esVacia().
+     */
     protected void pruebaEsVacia() {
         assert cola.esVacia() : "Cola debe ser vacía";
-        cola.poner('a');
-        assert cola.esVacia() == false : "Cola no debe ser vacía";
+        cola.poner("Uno");
+        assert !cola.esVacia() : "Cola no debe ser vacía";
     }
 
+    /**
+     * Prueba lineales.dinamicas.Cola.vaciar().
+     */
     protected void pruebaVaciar() {
-        cola.poner('a');
-        cola.poner('b');
-        cola.poner('c');
+        cola.poner("Uno");
+        cola.poner("Dos");
+        cola.poner("Tres");
         cola.vaciar();
-        assert cola.esVacia() : "Cola debe ser vacía";
+        assert !cola.sacar() : "No debe sacar de cola vacía";
         assert cola.obtenerFrente() == null : "Frente de cola debe ser nulo";
+        assert cola.esVacia() : "Cola debe ser vacía";
     }
 
+    /**
+     * Prueba lineales.dinamicas.Cola.clonar().
+     */
     protected void pruebaClonar() {
-        cola.poner('a');
-        cola.poner('b');
-        cola.poner('c');
-        Cola<Character> clon = cola.clonar();
-        assert cola.obtenerFrente() == clon.obtenerFrente()
-             : "Cola debe ser igual a su clon";
+        cola.poner("Uno");
+        cola.poner("Dos");
+        cola.poner("Tres");
+        Cola<String> clon = cola.clonar();
+        assert cola.obtenerFrente().equals(clon.obtenerFrente())
+             : "Frente de cola debe ser igual al de su clon (\"Uno\")";
         cola.sacar();
         clon.sacar();
-        assert cola.obtenerFrente() == clon.obtenerFrente()
-             : "Cola debe ser igual a su clon";
+        assert cola.obtenerFrente().equals(clon.obtenerFrente())
+             : "Frente de cola debe ser igual al de su clon (\"Dos\")";
         cola.sacar();
         clon.sacar();
-        assert cola.obtenerFrente() == clon.obtenerFrente()
-             : "Cola debe ser igual a su clon";
+        assert cola.obtenerFrente().equals(clon.obtenerFrente())
+             : "Frente de cola debe ser igual al de su clon (\"Tres\")";
         cola.sacar();
         clon.sacar();
         assert cola.esVacia() && clon.esVacia()
              : "Cola y su clon deben ser vacías";
     }
 
+    /**
+     * Prueba lineales.dinamicas.Cola.toString().
+     */
     protected void pruebaToString() {
-        cola.poner('a');
-        cola.poner('b');
-        cola.poner('c');
-        assert cola.toString().equals("[a b c]")
-             : "Cola con elementos 'a', 'b', 'c' debe ser representada como "
-             + "[a b c] en forma de cadena";
+        cola.poner("Uno");
+        cola.poner("Dos");
+        cola.poner("Tres");
+        assert cola.toString().equals("[Uno, Dos, Tres]")
+             : "Cola con elementos \"Uno\", \"Dos\", \"Tres\" debe ser "
+             + "representada como [Uno, Dos, Tres] en forma de cadena";
     }
 }
