ba6c5139c352cee065c9a92d6b434e5ddebb8971
Diego P. M. Baltar <dpmbaltar@gmail.com>
2019-07-20 23:24:42 -0300

    Actualizado NodoVertice y NodoAdyacente

--- src/grafos/dinamicas/NodoAdyacente.java
+++ src/grafos/dinamicas/NodoAdyacente.java
@@ -1,41 +1,82 @@
 package grafos.dinamicas;
 
 /**
- * Implementación de Nodo Adyacente.
+ * Implementación de Nodo Adyacente para un elemento de tipo T.
  *
  * @author Diego P. M. Baltar <dpmbaltar@gmail.com>
  * @param <T>
  */
 public class NodoAdyacente<T> {
 
+    /**
+     * El vértice del nodo adyacente.
+     */
     private NodoVertice<T> vertice;
+
+    /**
+     * El nodo adyacente siguiente.
+     */
     private NodoAdyacente<T> siguienteAdyacente;
 
+    /**
+     * Constructor vacío.
+     */
     public NodoAdyacente() {
         this(null, null);
     }
 
+    /**
+     * Constructor con el nodo vértice.
+     *
+     * @param vertice el nodo vértice
+     */
     public NodoAdyacente(NodoVertice<T> vertice) {
         this(vertice, null);
     }
 
+    /**
+     * Constructor con el nodo vértice y el siguiente nodo adyacente.
+     *
+     * @param vertice el nodo vértice
+     * @param siguienteAdyacente el nodo adyacente siguiente
+     */
     public NodoAdyacente(NodoVertice<T> vertice, NodoAdyacente<T> siguienteAdyacente) {
         this.vertice = vertice;
         this.siguienteAdyacente = siguienteAdyacente;
     }
 
+    /**
+     * Devuelve el nodo vértice.
+     *
+     * @return el nodo vértice
+     */
     public NodoVertice<T> getVertice() {
         return vertice;
     }
 
+    /**
+     * Establece el nodo vértice.
+     *
+     * @param vertice el nuevo nodo vértice
+     */
     public void setVertice(NodoVertice<T> vertice) {
         this.vertice = vertice;
     }
 
+    /**
+     * Devuelve el nodo adyacente siguiente.
+     *
+     * @return el nodo adyancente
+     */
     public NodoAdyacente<T> getSiguienteAdyacente() {
         return siguienteAdyacente;
     }
 
+    /**
+     * Establece el nodo adyacente siguiente.
+     *
+     * @param siguienteAdyacente el nodo adyacente
+     */
     public void setSiguienteAdyacente(NodoAdyacente<T> siguienteAdyacente) {
         this.siguienteAdyacente = siguienteAdyacente;
     }
--- src/grafos/dinamicas/NodoVertice.java
+++ src/grafos/dinamicas/NodoVertice.java
@@ -1,50 +1,111 @@
 package grafos.dinamicas;
 
+import lineales.dinamicas.Lista;
+
 /**
- * Implementación de Nodo Vértice.
+ * Implementación de Nodo Vértice para un elemento de tipo T.
  *
  * @author Diego P. M. Baltar <dpmbaltar@gmail.com>
  * @param <T>
  */
 public class NodoVertice<T> {
 
+    /**
+     * El elemento del vértice.
+     */
     private T elemento;
+
+    /**
+     * El primer nodo adyacente.
+     */
     private NodoAdyacente<T> primerAdyacente;
+
+    /**
+     * El siguiente nodo vértice.
+     */
     private NodoVertice<T> siguienteVertice;
 
+    /**
+     * Constructor con el elemento del vértice.
+     *
+     * @param elemento el elemento
+     */
     public NodoVertice(T elemento) {
         this(elemento, null);
     }
 
+    /**
+     * Constructor con el elemento del vértice y el siguiente vértice.
+     *
+     * @param elemento el elemento
+     * @param siguienteVertice el vértice siguiente
+     */
     public NodoVertice(T elemento, NodoVertice<T> siguienteVertice) {
         this.elemento = elemento;
         this.siguienteVertice = siguienteVertice;
     }
 
+    /**
+     * Devuelve el elemento del nodo vértice.
+     *
+     * @return el elemento
+     */
     public T getElemento() {
         return elemento;
     }
 
+    /**
+     * Establece el elemento.
+     *
+     * @param elemento el elemento
+     */
     public void setElemento(T elemento) {
         this.elemento = elemento;
     }
 
+    /**
+     * Devuelve el primer nodo adyacente.
+     *
+     * @return el nodo adyacente
+     */
     public NodoAdyacente<T> getPrimerAdyacente() {
         return primerAdyacente;
     }
 
+    /**
+     * Establece el primer nodo adyacente.
+     *
+     * @param primerAdyacente el nodo adyacente
+     */
     public void setPrimerAdyacente(NodoAdyacente<T> primerAdyacente) {
         this.primerAdyacente = primerAdyacente;
     }
 
+    /**
+     * Devuelve el nodo vértice siguiente.
+     *
+     * @return el nodo vértice
+     */
     public NodoVertice<T> getSiguienteVertice() {
         return siguienteVertice;
     }
 
+    /**
+     * Establece el nodo vértice siguiente.
+     *
+     * @param siguienteVertice el nodo vértice
+     */
     public void setSiguienteVertice(NodoVertice<T> siguienteVertice) {
         this.siguienteVertice = siguienteVertice;
     }
 
+    /**
+     * Verifica si el elemento de este nodo es igual al de otro nodo.
+     * <p>Agregado para localizar nodos en una lista, necesario en {@link Grafo#caminoMasCorto}.</p>
+     *
+     * @see Grafo#caminoMasCorto
+     * @see Lista#localizar
+     */
     @Override
     public boolean equals(Object o) {
         return elemento.equals(((NodoVertice<T>) o).getElemento());
