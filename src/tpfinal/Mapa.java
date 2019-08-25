99cfc7322c5766ea390af996d927414534bd9f00
Diego P. M. Baltar <dpmbaltar@gmail.com>
2019-08-25 00:39:43 -0300

    Actualizado Mapa

--- src/tpfinal/Mapa.java
+++ src/tpfinal/Mapa.java
@@ -1,11 +1,55 @@
 package tpfinal;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import grafos.dinamicas.Grafo;
 import grafos.dinamicas.NodoAdyacente;
 import grafos.dinamicas.NodoVertice;
 import lineales.dinamicas.Lista;
 
+/**
+ * Mapa del juego.
+ *
+ * @author Diego P. M. Baltar {@literal <dpmbaltar@gmail.com>}
+ */
 public class Mapa extends Grafo<String, Integer> {
+
+    /**
+     * Cantidad de locaciones en el mapa.
+     */
+    private int cantidadLocaciones;
+
+    /**
+     * Constructor vacío.
+     */
+    public Mapa() {
+        super();
+        cantidadLocaciones = 0;
+    }
+
+    @Override
+    public boolean insertarVertice(String locacion) {
+        boolean insertado = false;
+
+        if (super.insertarVertice(locacion)) {
+            insertado = true;
+            cantidadLocaciones++;
+        }
+
+        return insertado;
+    }
+
+    @Override
+    public boolean eliminarVertice(String locacion) {
+        boolean eliminado = false;
+
+        if (super.eliminarVertice(locacion)) {
+            eliminado = true;
+            cantidadLocaciones--;
+        }
+
+        return eliminado;
+    }
 
     /**
      * Devuelve una lista con las locaciones adyacentes a una locación dada.
@@ -27,5 +71,27 @@ public class Mapa extends Grafo<String, Integer> {
         }
 
         return adyacentes;
+    }
+
+    /**
+     * Devuelve una locación aleatoria.
+     *
+     * @return la locación obtenida
+     */
+    public String locacionAleatoria() {
+        String locacion = null;
+
+        if (!esVacio()) {
+            int aleatorio = ThreadLocalRandom.current().nextInt(0, cantidadLocaciones - 1);
+            NodoVertice<String, Integer> nodo = inicio;
+
+            for (int i = 0; i < aleatorio; i++) {
+                nodo = nodo.getSiguienteVertice();
+            }
+
+            locacion = nodo.getElemento();
+        }
+
+        return locacion;
     }
 }
